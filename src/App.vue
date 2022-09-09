d1f2017c80ec72609bf354dd881dd5ef034f8e33
DatouCode <datoucode@gmail.com>
2022-09-09 11:53:57 +0800

    paste

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
-  <a-upload
-      v-model:file-list="fileList"
-      name="file"
-      action="http://127.0.0.1:5000/upload"
-      @change="handleChange"
-  >
-    <a-button>
-      <upload-outlined></upload-outlined>
-      Click to Upload
-    </a-button>
-  </a-upload>
-  <br>
-  <span>{{ url }}</span>
+  <div>
+    <div class="box" @paste="pasting" contenteditable="true"></div>
+    <a :href="url">{{ url }}</a>
+  </div>
 </template>
 <script>
+import axios from 'axios'
 import {message} from 'ant-design-vue';
 import {UploadOutlined} from '@ant-design/icons-vue';
-import {defineComponent, ref} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 
 export default defineComponent({
   components: {
@@ -41,13 +33,45 @@ export default defineComponent({
       }
     };
 
+    const upload = async (str) => {
+      let {data} = await axios.post("http://localhost:5000/upload", {
+        base64: str
+      })
+      return data
+    }
+
+    const pasting = (e) => {
+      // console.log(e.clipboardData.getData("text"))
+      let file = e.clipboardData.items[0].getAsFile()
+      let reader = new FileReader()
+      reader.onload = async (e) => {
+        console.log(e.target)
+        let result = e.target.result
+        url.value = await upload(result)
+        console.log(url)
+      }
+      reader.readAsDataURL(file)
+    }
+
     const fileList = ref([]);
     return {
       url,
       fileList,
       handleChange,
+      pasting
     };
   },
 
 });
 </script>
+
+<style>
+.box {
+  width: 500px;
+  height: 500px;
+  border: 1px solid;
+  position: relative;
+  float: left;
+  overflow: hidden;
+}
+</style>
